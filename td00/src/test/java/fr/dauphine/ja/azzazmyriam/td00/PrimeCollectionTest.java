1a66a67ae20afc200b88295bc565c4359d62f8f7
azzamy16 <azzamy16@b032-06.crio.dauphine.fr>
2019-10-14 19:05:12 +0200

    Class test added

---
+++ td00/src/test/java/fr/dauphine/ja/azzazmyriam/td00/PrimeCollectionTest.java
@@ -0,0 +1,50 @@
+package fr.dauphine.ja.azzazmyriam.td00;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PrimeCollectionTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PrimeCollectionTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PrimeCollectionTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testPrimeCollection()
+    {
+        assertTrue( true );
+    }
+    
+    public void test0IsPrime() {
+    	assertFalse(PrimeCollection.isPrime(0));
+    }
+    
+	public void testTwoIsPrime() {
+	    assertTrue(PrimeCollection.isPrime(2));	
+	}
+	
+	public void test9IsNotPrime() {
+		assertFalse(PrimeCollection.isPrime(9));
+	}
+}
